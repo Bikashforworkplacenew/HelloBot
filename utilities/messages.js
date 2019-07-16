6e9f285774f61a170cefb20fe46d26ea52f656c0
Bikash Rajguru <bikashrajguru@fb.com>
2019-07-16 13:14:43 +0800

    sassa

--- utilities/messages.js
+++ utilities/messages.js
@@ -18,7 +18,7 @@ module.exports = function(graph_api){
   //Handle received message
   module._handleMessage = function(message) {
     let senderID = message.sender.id;
-    this._sendMessage(senderID, "Bikash!");
+    this._sendMessage(senderID, "Hello KJK!");
   }
 
   //Send message from the bot to the user
